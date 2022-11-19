ac279d2725e5d710581d0d3463ea07225f437dfe
Zagitalc <zachary.13281@gmail.com>
2022-11-19 17:28:31 +0000

    1

--- Hackathon.py
+++ Hackathon.py
@@ -35,7 +35,7 @@ examples = [
     Example("", "Toxic"),
     Example("", "Toxic"),
     Example("", "Toxic"),
-    Example("", "Toxic"),
+    Example("",  "Toxic"),
     Example("", "Benign"),
     Example("", "Benign"),
     Example("", "Benign"),
@@ -43,17 +43,20 @@ examples = [
     Example("", "Benign"),
     Example("", "Benign"),
     Example("", "Benign"),
-    Example("", "Benign"),
-    Example("", "Benign"),
-    Example("", "Benign"),
-    Example("", "Benign"),
-    Example("", "Benign"),
-    Example("", "Benign"),
-    Example("", "Benign"),
-    Example("", "Benign"),
-    Example("", "Benign"),
-    Example("", "Benign"),
-    Example("", "Benign")
+    # zac
+    Example("I appreciate the video.", "Benign"),
+    Example("Happy birthday will! ", "Benign"),
+    Example(
+        "You are a wise man, Will. We all felt with You during those days. ", "Benign"),
+    Example("Fuck you", "Toxic"),
+    Example("Peace to you man", "Benign"),
+    Example("Idk if ur reading this but ur amazing dude. It’s just ur family dude they don’t deserve u they are pulling u down.", "Benign"),
+    Example("Love you Will and praying for you", "Benign"),
+    Example("It’s not how you fall, it’s how you get back up", "Benign"),
+    Example(
+        "It takes 20 years to build a reputation and five minutes to ruin it", "Toxic"),
+    Example("A mistake? That’s a career defining moment that will not easily be forgotten—ever.", "Toxic"),
+    Example("This is EXACTLY how he apologizes when he fuxks up. It's eerie.", "Toxic")
 ]
 
 inputs = [
