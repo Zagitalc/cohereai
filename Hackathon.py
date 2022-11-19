f6c8125ad0fc9028a315b1bc9982e6260c2ecaef
KinTs145 <rmjq45@durham.ac.uk>
2022-11-19 15:21:27 +0000

    1

---
+++ Hackathon.py
@@ -0,0 +1,35 @@
+import cohere
+co = cohere.Client('lFHIj2hW8tNMFa1oDPUspENDrDHAWDV12ob8YMvO')
+
+from cohere.classify import Example
+
+examples = [
+  Example("Ng Tsz Lok", "Toxic"),  
+  Example("you are hot trash", "Toxic"),  
+  Example("go to hell", "Toxic"),
+  Example("get rekt moron", "Toxic"),  
+  Example("get a brain and use it", "Toxic"), 
+  Example("say what you mean, you jerk.", "Toxic"), 
+  Example("Are you really this stupid", "Toxic"), 
+  Example("I will honestly kill you", "Toxic"),  
+  Example("yo how are you", "Benign"),  
+  Example("I'm curious, how did that happen", "Benign"),  
+  Example("Try that again", "Benign"),  
+  Example("Hello everyone, excited to be here", "Benign"), 
+  Example("I think I saw it first", "Benign"),  
+  Example("That is an interesting point", "Benign"), 
+  Example("I love this", "Benign"), 
+  Example("We should try that sometime", "Benign"), 
+  Example("You should go for it", "Benign")
+]
+
+inputs =["go to hell","Ng Tsz Lok"]
+response = co.classify(
+    model='medium',
+    inputs= inputs,
+    examples = examples
+)
+
+print(response.classifications)
+
+
